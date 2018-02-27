2f68623bde1f015ec0b7cedae07a12631373195f
E3V3A <xdae3v3a@gmail.com>
2018-02-27 18:12:22 +0200

    :zip:

---
+++ tools/getplanes.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+'use strict'
+const radar = require('flightradar24-client/lib/radar')
+
+// A no planes place:
+//radar(83, 13, 82, 14)
+// "DPS" @ 60 km (Bali Airport)
+//radar(-8.20917,114.62177,-9.28715,115.71243)
+// 60 km @ (Berlin)
+radar(53.05959,12.52388,51.98161,14.29552)
+.then(console.log)
+.catch(console.error)
